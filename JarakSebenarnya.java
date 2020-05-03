fb47beb79b745f6c2c33ea4e47597236f610011a
Evinuria <63250961+Evinuria@users.noreply.github.com>
2020-05-03 11:26:22 +0700

    Add files via upload

--- JarakSebenarnya.java
+++ JarakSebenarnya.java
@@ -1,48 +1,38 @@
 import java.util.Scanner ;
-public class JarakSebenarnya{
+public class JarakSebenarnya {
     String pilih;
     int JP;
     int JS;
     int Skala;
-    String lagi;
 
     Scanner input = new Scanner (System.in);
-    public void pilihan(){
-        
-        System.out.println("Pilih nomer berapa = ");
-        pilih = input.nextLine();
-        switch(pilih){
+    public void pilihan () {
+        System.out.println ( "Pilih nomer berapa = " );
+        pilih = input.nextLine ();
+        switch ( pilih ) {
             case "1" :
-                System.out.println("Masukkan Jarak Pada Peta (cm) = ");
-                JP = input.nextInt();
-                System.out.println("Masukkan Skala = 1 : ");
-                Skala = input.nextInt();
+                System.out.println ( "Masukkan Jarak Pada Peta (cm) = " );
+                JP = input.nextInt ();
+                System.out.println ( "Masukkan Skala = 1 : " );
+                Skala = input.nextInt ();
                 JS = JP*Skala ;
-                System.out.println ("Jarak Sebenarnya adalah = " + JS + " cm");
-                
-                System.out.print("Kembali ke menu utama? (Y/T)  =   ");
-                Scanner x = new Scanner(System.in);
-                lagi = x.nextLine().toUpperCase();
+                System.out.println ( "Jarak Sebenarnya adalah = " + JS + " cm" );
                 break;
 
             case "2" :
-                System.out.println("Masukkan Jarak Peta (JP) = ");
-                JP = input.nextInt();
-                System.out.println("Masukkan Jarak Sebernarnya (JS) = ");
-                JS = input.nextInt();
+                System.out.println ( "Masukkan Jarak Peta (cm) = " );
+                JP = input.nextInt ();
+                System.out.println ( "Masukkan Jarak Sebernarnya (cm) = " );
+                JS = input.nextInt ();
                 int Jp = 1 ; 
             
                 Skala = JS / JP; 
-                System.out.println("Skala adalah = " + Jp + ":" + Skala);
-
-                System.out.print("Kembali ke menu utama? (Y/T)  =   ");
-                Scanner X = new Scanner(System.in);
-                lagi = X.nextLine().toUpperCase();
+                System.out.println ( "Skala adalah = " + Jp + ":" + Skala );
                 break;
                 
             case "3" :
-                System.out.println("Hanya ada 2 pilihan");
+                System.out.println ( "Hanya ada 2 pilihan" );
                 break;
-            }
         }
+    }
 }
--- main.java
+++ main.java
@@ -1,22 +1,30 @@
 import java.util.Scanner ;
-public class main extends JarakSebenarnya{
-    public static void main ( String args[]){
-        Scanner input = new Scanner (System.in);
-        System.out.println("HELLO");
-        System.out.println("MY NAME IS EVINURIA FILUYUNISTIARA");
-        System.out.println("Today we are going to learn about");
-        System.out.println("JARAK SEBENARNYA DAN SKALA");
+public class main extends JarakSebenarnya {
+    public static void main ( String args[] ) {
+        Scanner input = new Scanner (System.in) ;
+        System.out.println ( "HELLO" );
+        System.out.println ( "MY NAME IS EVINURIA FILUYUNISTIARA" );
+        System.out.println ( "Today we are going to learn about" );
+        System.out.println ( "JARAK SEBENARNYA DAN SKALA" );
         
         JarakSebenarnya main;
-        main = new JarakSebenarnya();
-        String lagi = "Y";
-        while(lagi.equals("Y")){
-        System.out.println("1. Mencari Jarak Sebenarnya");
-        System.out.println("2. Mencari Skala");
-        main.pilihan();}
+        main = new JarakSebenarnya ();
+        boolean cek = true;
+        while ( cek ){
+        System.out.println ( "1. Mencari Jarak Sebenarnya" );
+        System.out.println ( "2. Mencari Skala" );
+        main.pilihan ();
         
-        while(lagi.equals("T")){
-        System.out.println("Sampai Jumpa");
+            System.out.print ( "Kembali ke menu utama? (Y/T)  =   " );
+            String coba = input.nextLine ();
+                if ( coba.equals ( "Y" ) ){
+                    cek = true;
+                    main.pilihan();
+                }
+                else if ( coba.equals ( "T" ) ){
+                    cek = false;
+                    System.out.println ( "Sampai Jumpa" );
+            }
+        }
     }
-  }
 }
